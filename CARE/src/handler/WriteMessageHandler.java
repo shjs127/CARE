be1fcc5369db29792e96cfdd7c885e5510623bd0
shjs127 <shjs127@hanmail.net>
2021-06-26 15:34:35 +0900

    리뷰에 userno,storeno 데이터 받아오기

--- CARE/src/handler/WriteMessageHandler.java
+++ CARE/src/handler/WriteMessageHandler.java
@@ -1,17 +1,24 @@
 package handler;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import auth.service.LoginFailException;
 import auth.service.Message;
+import auth.service.StoreInfoService;
+import auth.service.User;
 import auth.service.WriteMessageService;
+import member.model.StoreInfo;
+import member.model.UserInfo;
 import mvc.command.CommandHandler;
 
 public class WriteMessageHandler implements CommandHandler {
 
 	private static final String FORM_VIEW = "/WEB-INF/view/main/food-details.jsp";
-
+	private StoreInfoService storeinfoService = new StoreInfoService();
 	@Override
 	public String process(HttpServletRequest req, HttpServletResponse res) throws Exception {
 		if (req.getMethod().equalsIgnoreCase("GET")) {
@@ -34,20 +41,31 @@ public class WriteMessageHandler implements CommandHandler {
 
 	private String processSubmit(HttpServletRequest req, HttpServletResponse res) throws Exception {
 		Message message = new Message();
-
+		User user = (User) req.getSession().getAttribute("authUser");
+		
+		String reviewC=req.getParameter("reviewContents");
 		message.setReviewContents(req.getParameter("reviewContents"));
 		double avgScore = Double.parseDouble(req.getParameter("avgScore"));
 		message.setAvgScore(avgScore);
 		String storeNo = req.getParameter("storeNo");
-
+		int storeNoLog = Integer.parseInt(storeNo);
+		
+		Map<String, Boolean> errors = new HashMap<>();
+		req.setAttribute("errors", errors);
+		
+		if ((reviewC) == null || reviewC.isEmpty())
+			errors.put("id", Boolean.TRUE);
+		
 		System.out.println("message.reveiewContents=" + message.getReviewContents());
 		System.out.println("message.AvgScore=" + message.getAvgScore());
 		System.out.println("storeNo="+storeNo);
+		
+
 
 		try {
 
 			WriteMessageService writeService = WriteMessageService.getInstance();
-			 writeService.writeMessage(message);
+			 writeService.writeMessage(message,user.getUserNo(),storeNoLog);
 			
 			res.sendRedirect("foodDetail.do?storeno="+storeNo);
 			return null;
